8a0334569cc461f79698f69d76b18c8a8a67bbb1
msgc <Kentaro Miuchi>
2021-09-10 17:22:23 +0900

    add DAC reset (-r) option to MADA_SetDAC.py

---
+++ bin/MADA_SetDAC.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+import subprocess, os,sys
+import argparse
+import json
+import datetime
+
+MADAPATH="/home/msgc/miraclue/MADA/bin/"
+CONFIGPATH="/home/msgc/miraclue/MADA/config/"
+
+LOGPATH="DAClog/"
+CONFIG="MADA_config.json"
+DAC_null="base_correct_null.dac"
+
+FETCHCON="fetch_config.py"
+
+SETVth_EXE=MADAPATH+"SetVth"
+SETDAC_EXE=MADAPATH+"SetDAC"
+READMEM_EXE=MADAPATH+"read_CtrlMem"
+
+reset=0
+
+#def parser():
+parser=argparse.ArgumentParser()
+#    argparser.add_argument("MADA_config_file",type=str,nargs='?',const=None,help='MADA config file')
+parser.add_argument("-r",help="reset DACs",action='store_true')
+args=parser.parse_args()
+reset=args.r
+parser.add_argument("-c",help="config file name",default=CONFIG)
+args = parser.parse_args( )
+CONFIG=args.c
+#return(opts)
+
+
+def p_and_e(cmd):
+    print("execute:"+cmd)
+    subprocess.run(cmd,shell=True)
+
+
+#parser = argparse.ArgumentParser()
+#parser.add_argument("-c",help="config file name",default=CONFIG)
+#args = parser.parse_args( )
+#CONFIG=args.c
+
+#print("DAC config file:",DAC_CONFIG)
+
+
+if(os.path.isdir(LOGPATH)==False):
+   cmd="mkdir "+LOGPATH
+   subprocess.run(cmd,shell=True)
+   
+if(os.path.isfile(CONFIG)):
+   print(CONFIG," exists.")
+else:
+   cmd=MADAPATH+FETCHCON
+   subprocess.run(cmd,shell=True)
+      
+
+with open(CONFIG) as f:
+    d = json.load(f)
+
+
+for i in d['gigaIwaki']:
+#    name=d['gigaIwaki'][i]['board name']
+    IP=d['gigaIwaki'][i]['IP']
+    Vth=d['gigaIwaki'][i]['Vth']
+    DACfile=d['gigaIwaki'][i]['DACfile']
+    if(reset):
+        DACfile=CONFIGPATH+"base_correct_null.dac"
+        
+    #DAC and Vth writing
+    cmd=SETDAC_EXE+" "+IP+" "+DACfile
+    p_and_e(cmd)
+    #    print(cmd)
+    cmd=SETVth_EXE+" "+IP+" "+str(Vth)
+    #    print(cmd)
+    p_and_e(cmd)
+ #   i+=1;
+
+
+#i=0
+#for x in d:
+    #memory check and log
+    dt=datetime.datetime.now()
+    flog=LOGPATH+str(dt.year)+str(dt.month).zfill(2)+str(dt.day).zfill(2)+"-"+str(dt.hour).zfill(2)+str(dt.minute).zfill(2)+str(dt.second).zfill(2)
+    with open(flog,'w') as log_out:
+        cmd=READMEM_EXE+" "+IP
+        subprocess.run(cmd,shell=True,stdout=log_out)       
+    print("memory check log:"+flog)    
+#    i+=1;
+
+#print("SetAllDAC.py [DAC config json file]")
+#print("config file:",CONFIG)
---
+++ bin/fetch_config.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import os, sys
+import subprocess
+import numpy
+import glob
+import time
+import argparse
+import json
+from subprocess import PIPE
+
+MADAPATH="/home/msgc/miraclue/MADA/bin/"
+findADALM="findADALM2000.py"
+CONFIG="MADA_config.json"
+CONFIG_SKEL="MADA_config_SKEL.json"
+
+if(os.path.isfile(CONFIG)):
+    print(CONFIG+" exists.")
+else:
+    # make config file from skelton file
+    CONFIG_SKEL=MADAPATH+CONFIG_SKEL
+    print("\tMADA config slkelton file: "+CONFIG_SKEL)
+    skel_open= open(CONFIG_SKEL,'r')
+    skel_load = json.load(skel_open)
+
+    #set ADALM URIs by checking S/Ns
+    for x in skel_load['ADALM']:
+        SN=skel_load['ADALM'][x]['S/N']
+        cmd=MADAPATH+findADALM+" "+SN
+        proc=subprocess.run(cmd,shell=True,stdout=PIPE,stderr=None,check=False,capture_output=False)
+        URI=proc.stdout.decode("utf8").replace("\n","")
+        print("\tURI in "+CONFIG_SKEL+" "+skel_load['ADALM'][x]['URI']+" for S/N: "+SN)
+        skel_load['ADALM'][x]['URI']=URI
+        print("\tnew URI in "+CONFIG+" "+skel_load['ADALM'][x]['URI']+" for S/N: "+SN)    
+    
+        with open(CONFIG, mode='wt', encoding='utf-8') as file:
+            json.dump(skel_load, file, ensure_ascii=False, indent=2)
+
+#prepare config file ends.
+
