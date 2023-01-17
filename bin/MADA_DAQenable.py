b7d798bb7e2f435295b39c78e7e153f99f77302f
hmdyt <konnyaku108@gmail.com>
2023-01-17 19:01:24 +0900

    format

--- bin/MADA_DAQenable.py
+++ bin/MADA_DAQenable.py
@@ -1,43 +1,38 @@
 #!/usr/bin/python3
 
-import subprocess
 import argparse
 import subprocess
 from subprocess import PIPE
 
-MADAPATH="/home/msgc/miraclue/MADA/bin/"
-ADAPATH="/home/msgc/adalm/adalm_out"
+MADAPATH = "/home/msgc/miraclue/MADA/bin/"
+ADAPATH = "/home/msgc/adalm/adalm_out"
 
-#scripts
-FETCHCON="MADA_fetch_config.py"
-findADALM="findADALM2000.py"
-SETDAC="MADA_SetAllDACs.py"
+# scripts
+FETCHCON = "MADA_fetch_config.py"
+findADALM = "findADALM2000.py"
+SETDAC = "MADA_SetAllDACs.py"
 
-#configs
-CONFIG="MADA_config.json"
-CONFIG_SKEL="MADA_config_SKEL.json"
+# configs
+CONFIG = "MADA_config.json"
+CONFIG_SKEL = "MADA_config_SKEL.json"
 
-#def getURI
+SN = "10447384b904001612002500df1edb6193"  # adalm0
+cmd = MADAPATH+findADALM+" "+SN
+proc = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=None, check=False, capture_output=False)
+URI = proc.stdout.decode("utf8").replace("\n", "")
+print("\tURI " + URI + "for S/N: "+SN)
 
-#URI="usb:1.7.5"
-#SN="104473961406000712000e0056e64887db"
-SN="10447384b904001612002500df1edb6193"
-cmd=MADAPATH+findADALM+" "+SN
-proc=subprocess.run(cmd,shell=True,stdout=PIPE,stderr=None,check=False,capture_output=False)
-URI=proc.stdout.decode("utf8").replace("\n","")
-print("\tURI " +URI+ "for S/N: "+SN)      
-disable=0;
+disable = 0
 parser = argparse.ArgumentParser()
-parser.add_argument("-d",help="disable",action='store_true')
-parser.add_argument("-u",help="ADALM URI",default=URI)
-args = parser.parse_args( )
-#rate=args.f
-URI=args.u
-disable=args.d
-if disable: # latching down
-    com=ADAPATH+"/bin/ad_out -u "+URI+" -m"
+parser.add_argument("-d", help="disable", action='store_true')
+parser.add_argument("-u", help="ADALM URI", default=URI)
+args = parser.parse_args()
+URI = args.u
+disable = args.d
+if disable:  # latching down
+    com = ADAPATH+"/bin/ad_out -u "+URI+" -m"
 else:
-    com=ADAPATH+"/bin/ad_out -u "+URI+" -l"
+    com = ADAPATH+"/bin/ad_out -u "+URI+" -l"
 
 print(com)
-subprocess.run(com,shell=True)
+subprocess.run(com, shell=True)
